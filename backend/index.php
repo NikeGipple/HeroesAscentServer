f8d565cb9f431c3171b33d3eb0d90d548e45b909
NikeGipple <NikeGipple>
2025-11-02 17:06:46 +0100

    redo new api

--- backend/index.php
+++ backend/index.php
@@ -4,13 +4,8 @@ header('Content-Type: application/json; charset=utf-8');
 $rules_valid = false;
 $violation_code = "RULE_FOOD_001";
 
-$output = json_encode([
+echo json_encode([
     "status" => "ok",
     "rules_valid" => $rules_valid,
     "violation_code" => $violation_code
 ]);
-
-// Rimuove tutti gli spazi dopo i due punti
-$output = str_replace(': ', ':', $output);
-
-echo $output;
