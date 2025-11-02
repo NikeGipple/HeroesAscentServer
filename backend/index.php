69ca304657b590672f3d60871b526ace41bd082f
NikeGipple <NikeGipple>
2025-11-02 16:47:53 +0100

    new api

--- backend/index.php
+++ backend/index.php
@@ -1,13 +1,11 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-// esempio: nessuna violazione
 $rules_valid = false;
 $violation_code = "RULE_FOOD_001";
 
-
 echo json_encode([
-  "status" => "ok",
-  "rules_valid" => $rules_valid,
-  "violation_code" => $violation_code
+    "status" => "ok",
+    "rules_valid" => $rules_valid,
+    "violation_code" => $violation_code
 ]);
