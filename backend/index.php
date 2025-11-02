4d4617517b6cf2b5a4df67355fafc1951d9130ea
NikeGipple <NikeGipple>
2025-11-02 16:58:54 +0100

    new api 2

--- backend/index.php
+++ backend/index.php
@@ -4,8 +4,13 @@ header('Content-Type: application/json; charset=utf-8');
 $rules_valid = false;
 $violation_code = "RULE_FOOD_001";
 
-echo json_encode([
+$output = json_encode([
     "status" => "ok",
     "rules_valid" => $rules_valid,
     "violation_code" => $violation_code
 ]);
+
+// Rimuove tutti gli spazi dopo i due punti
+$output = str_replace(': ', ':', $output);
+
+echo $output;
