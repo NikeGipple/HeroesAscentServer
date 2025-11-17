a5e1d27665629f716302b7e6b778e00479128ae9
NikeGipple <NikeGipple>
2025-11-17 22:36:20 +0100

    β - 0.044.20251117

--- database/seeders/BannedSkillsSeeder.php
+++ database/seeders/BannedSkillsSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
+use App\Services\Gw2ApiService;
+
 
 class BannedSkillsSeeder extends Seeder
 {
